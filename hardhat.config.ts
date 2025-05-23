9c123962468d3846ba75b7b554cb46ddde153ac7
SampleApp05 <sampleapp05@gmail.com>
2025-05-23 10:00:58 +0300

    Configure projects and add dependency list

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,37 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      //evmVersion: "cancun", // seems to be required for the latest hardhat || will use Paris evm
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./src", // Contracts location
+    tests: "./hardhat-tests", // Hardhat tests
+    artifacts: "./artifacts",
+    cache: "./cache",
+  },
+  defaultNetwork: "localhost",
+  networks: {
+    sepolia: {
+      url: `${process.env.SEPOLIA_RPC_URL}`,
+      accounts: process.env.WALLET_PRIVATE_KEY
+        ? [`0x${process.env.WALLET_PRIVATE_KEY}`]
+        : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || "",
+  },
+  sourcify: {
+    enabled: true,
+  },
 };
 
 export default config;
